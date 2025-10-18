6879536662c5b5f981e28f70d0449b061109cc0b
Nazim <i499773@fontysict.nl>
2025-10-18 13:48:24 +0200

    added verification method for loader.io

--- src/main/java/com/example/encryption_demo/EncryptionController.java
+++ src/main/java/com/example/encryption_demo/EncryptionController.java
@@ -30,4 +30,10 @@ public class EncryptionController {
             return ResponseEntity.badRequest().body(new ErrorResponse("Decryption failed: " + e.getMessage()));
         }
     }
+
+    @GetMapping("/loaderio-71cde7773e383aa12f1f5d1123308af8/")
+    public String loaderioVerification() {
+        return "loaderio-71cde7773e383aa12f1f5d1123308af8";
+    }
+
 }
